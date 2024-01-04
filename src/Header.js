89b8ebd9b48eaeab922bcd96aa785f9448357d2b
remelee <calie6972@gmail.com>
2024-01-04 13:52:21 -0500

    added header

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import './Header.css';
+import logo from './logo.svg';
+function Header(){
+    return(
+        <div className="header">
+            <img src='./logo.svg'></img>
+            About Projects Contact
+        </div>
+    )
+}
+export default Header
